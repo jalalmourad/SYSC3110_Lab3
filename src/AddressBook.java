841b7eea795c27ab9c0bba6ad468a033d592bc50
jalalmourad <114366973+jalalmourad@users.noreply.github.com>
2024-09-24 16:57:26 -0400

    Created Lab3

---
+++ src/AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList<BuddyInfo> buddies;
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if(buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+    }
+
+    public BuddyInfo removeBuddyIndex(int index){
+        if(index>=0 && index<buddies.size()){
+            return buddies.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        AddressBook ab = new AddressBook();
+        BuddyInfo buddy = new BuddyInfo("Jalal","Carleton",123);
+
+        System.out.println("Address Book:  ");
+
+        ab.addBuddy(buddy);
+        ab.removeBuddyIndex(0);
+
+
+    }
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,40 @@
+public class BuddyInfo {
+    String name;
+    String Address;
+    int phoneNumber;
+
+
+    public BuddyInfo(String name, String Address, int phoneNumber) {
+        this.name = name;
+        this.Address = Address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo(){
+        name = "Jalal";
+        Address = "222 Address";
+        phoneNumber = 11111;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy = new BuddyInfo("Homer","100 Kibi cres", 2222222);
+        System.out.println("Hello "+ buddy.getName());
+        System.out.println("Hello world!");
+    }
+}
